2d4c4af4cb721ce4bc5c766f9ebba318a4b37f58
Antonio Banda <antonio.banda.dev@gmail.com>
2025-05-31 18:24:38 -0600

    Update Module8P2

--- app/src/main/java/com/jabg/modulo6p2/ui/fragments/AlbumsFragment.kt
+++ app/src/main/java/com/jabg/modulo6p2/ui/fragments/AlbumsFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
